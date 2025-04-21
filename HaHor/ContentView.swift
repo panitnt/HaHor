cdc888031b507926b0dc12c235b14dc87b09cd7d
panitnt <panittatanya@gmail.com>
2025-04-21 14:07:00 +0700

    Initial Commit

---
+++ HaHor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HaHor
+//
+//  Created by bell on 21/4/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HaHor/HaHorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaHorApp.swift
+//  HaHor
+//
+//  Created by bell on 21/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct HaHorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
